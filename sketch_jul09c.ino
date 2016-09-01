0e7a314f6160d2a84cdaa6fc4281cde889655af3
0x5c <winhelp32.exe@gmail.com>
2016-08-31 22:37:14 -0400

    Corrected some errors

--- sketch_jul09c.ino
+++ sketch_jul09c.ino
@@ -45,7 +45,7 @@ void setup() {
 }
 
 void loop() {
-/* Totaly useless block of f*** code done by Alfredo Liguine
+/* Totaly useless block of f*** code by MaxMatteau
   digitalRead(2);
   digitalRead(3);
   digitalRead(4);
